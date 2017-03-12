be0f095d61aaa274680f6db32fdac8cacec18507
Irina O'Hara <irina.v.ohara@gmail.com>
2017-03-12 01:11:01 -0700

    fixed failures, wrote script according to the assignments

--- spec/features/oc_tanner/web_test/launch_octanner_spec.rb
+++ spec/features/oc_tanner/web_test/launch_octanner_spec.rb
@@ -8,6 +8,7 @@ feature test_name.to_s, sauce: app.sauce do
 
     app.launch_app(app.url('OCT'))
     app.oct_landing_page.open_menu
+    app.oct_landing_page.compare_links
     app.close_all_windows
   end
 end
--- spec/support/apps/modules/octanner_header.rb
+++ spec/support/apps/modules/octanner_header.rb
@@ -1,7 +1,16 @@
 module Header
 
   def open_menu
-    page.find[:css, "a[id='mobile-nav-link']"].click
+  	page.driver.browser.manage.window.resize_to(375,667)
+    page.find(:css, "a[id='mobile-nav-link']").click
+  end
+
+  def compare_links
+    page.all(:css, 'nav#mobile-nav-container > ul ').each do |li|
+      	li.click if href.to_s eql? a.text().to_s do
+      		puts "this is a correct page"
+      	end
+    end
   end
 
 end
